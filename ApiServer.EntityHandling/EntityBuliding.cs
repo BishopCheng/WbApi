24ae0a93146037614ea427011b763b2bae1dfcc8
chr <intothesun@163.com>
2018-10-16 19:19:53 +0800

    完善 EntityBuliding

--- ApiServer.EntityHandling/EntityBuliding.cs
+++ ApiServer.EntityHandling/EntityBuliding.cs
@@ -12,7 +12,7 @@ namespace ApiServer.EntityHandling
     /// 作者：程淮榕
     /// 时间：2018-10-15
     /// </summary>
-    public  class EntityBuliding
+    public static  class EntityBuliding
     {
         /// <summary>
         /// 读取模型
@@ -27,15 +27,23 @@ namespace ApiServer.EntityHandling
                     Type typeFromHandle = typeof(T);    //获取T的类型
                     int fieldCount = dataReaderObject.FieldCount;  //获取读取器的区域块数
                     T val = Activator.CreateInstance<T>();         //建立访问器
-                    for (int i = 0; i < fieldCount; i++)
+                    for (int i = 0; i < fieldCount;i++)
                     {
-                        if (ISNullOrDBNull(dataReaderObject[i]))   //如果读取器的区域块数不为空,则写入
+                        if (!ISNullOrDBNull(dataReaderObject[i]))   //如果读取器的区域块数不为空,则写入
                         {
-
+                            PropertyInfo propertyInfo = typeFromHandle.GetProperty(dataReaderObject.GetName(i), BindingFlags.IgnoreCase | BindingFlags.Instance |
+                            BindingFlags.Public | BindingFlags.GetProperty);
+                            if (propertyInfo != (PropertyInfo)null)
+                            {
+                                //将IDataReader读出来的数据写到实体对象的属性里
+                                propertyInfo.SetValue(val, CheckType(dataReaderObject[i], propertyInfo.PropertyType), null);
+                            }
                         }
+                       
                     }
-
+                    return val;
                 }
+                return default(T);
             }
         }
 
@@ -48,6 +56,27 @@ namespace ApiServer.EntityHandling
             if(objInput == null || objInput is DBNull)
             { return true; }
             else { return false; }
+        }
+
+
+        /// <summary>
+        /// 检查实体类型
+        /// </summary>
+        /// <param name="value">实体</param>
+        /// <param name="conversionType">类型</param>
+        /// <returns></returns>
+        private static object CheckType(Object value, Type conversionType)
+        {
+            if (conversionType.IsGenericType && conversionType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+            {
+                if (value == null)
+                {
+                    return null;
+                }
+                NullableConverter nullableConverter = new NullableConverter(conversionType);
+                conversionType = nullableConverter.UnderlyingType;
+            }
+            return Convert.ChangeType(value, conversionType);
         }
     }
 }
