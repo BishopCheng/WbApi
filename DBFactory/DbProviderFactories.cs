3ca80e709b56fb139f88e14bb807566fc47cf74e
chr <intothesun@163.com>
2018-11-07 19:02:17 +0800

    添加SQLSetting接口

--- DBFactory/DbProviderFactories.cs
+++ DBFactory/DbProviderFactories.cs
@@ -2,6 +2,9 @@
 using MySql.Data.MySqlClient;
 using Npgsql;
 using Oracle.ManagedDataAccess.Client;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.OracleClient;
 
 namespace DBFactory
 {
@@ -10,6 +13,91 @@ namespace DBFactory
     /// </summary>
     public static class DbProviderFactories
     {
+        /// <summary>
+        /// 获取数据库工厂实体
+        /// </summary>
+        /// <param name="provideName">工厂名称</param>
+        /// <returns></returns>
+        public static DbProviderFactory GetProviderFactory(string provideName)
+        {
+            if (!string.IsNullOrEmpty(provideName))
+            {
+                switch(provideName)
+                {
+                    case "MySql.Data.MySqlClient":                          //MySql
+                        return (DbProviderFactory)new MySqlClientFactory();
+                    case "System.Data.SqlClient":
+                        return (DbProviderFactory)SqlClientFactory.Instance;  //SQL
+                    case "Npgsql":
+                        return (DbProviderFactory)NpgsqlFactory.Instance;   //NPGSQL
+                    case "System.Data.OracleClient":
+                        return (DbProviderFactory)new OracleClientFactory();  //Oracle
+                    case "System.Data.OleDb":
+                        throw new Exception("暂不支持您使用的数据库类型！");
+                    case "System.Data.ODBC":
+                        throw new Exception("暂不支持您使用的数据库类型！");
+                    case "Oracle.DataAccess.Client":
+                        throw new Exception("暂不支持您使用的数据库类型！");
+                    case "System.Data.SQLite":
+                        throw new Exception("暂不支持您使用的数据库类型！");
+                    case "FirebirdSql.Data.Firebird":
+                        throw new Exception("暂不支持您使用的数据库类型！");
+                    case "IBM.Data.DB2.iSeries":
+                        throw new Exception("暂不支持您使用的数据库类型！");
+                    case "IBM.Data.Informix":
+                        throw new Exception("暂不支持您使用的数据库类型！");
+                    case "System.Data.SqlServerCe":
+                        throw new Exception("暂不支持您使用的数据库类型！");
+                    default:
+                        throw new Exception("暂不支持您使用的数据库类型！");
+                }
+            }
+            throw new Exception("数据库驱动类型配置不正确");
+        }
+
+        /// <summary>
+        /// 获取链接实体类
+        /// </summary>
+        /// <param name="providName">数据库驱动名称</param>
+        /// <param name="connectionString">链接字段</param>
+        /// <returns></returns>
+        public static DbConnection GetDbConnection(string providName,string connectionString)
+        {
+            if (!string.IsNullOrEmpty(providName))
+            {
+                switch (providName)
+                {
+                    case "MySql.Data.MySqlClient":                          //MySql
+                        return (DbConnection)new MySqlConnection(connectionString);
+                    case "System.Data.SqlClient":                          //SQLServer
+                        return (DbConnection)new SqlConnection(connectionString);
+                    case "Npgsql":                                         //Npgsql
+                        return (DbConnection)new NpgsqlConnection(connectionString);
+                    case "System.Data.OracleClient":                       //Oracle
+                        return (DbConnection)new OracleConnection(connectionString);
+                    case "System.Data.OleDb":
+                        throw new Exception("数据库驱动类型配置不正确！");
+                    case "System.Data.ODBC":
+                        throw new Exception("数据库驱动类型配置不正确！");
+                    case "Oracle.DataAccess.Client":
+                        throw new Exception("数据库驱动类型配置不正确！");
+                    case "System.Data.SQLite":
+                        throw new Exception("数据库驱动类型配置不正确！");
+                    case "FirebirdSql.Data.Firebird":
+                        throw new Exception("数据库驱动类型配置不正确！");
+                    case "IBM.Data.DB2.iSeries":
+                        throw new Exception("数据库驱动类型配置不正确！");
+                    case "IBM.Data.Informix":
+                        throw new Exception("数据库驱动类型配置不正确！");
+                    case "System.Data.SqlServerCe":
+                        throw new Exception("数据库驱动类型配置不正确！");
+                    default:
+                        throw new Exception("数据库驱动类型配置不正确！");
+                }
+                 
+            }
+            throw new Exception("数据库驱动类型配置不正确！");
+        }
 
     }
 }
---
+++ SQLSettings/Interface/ISetting.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLSettings.Interface
+{
+    /// <summary>
+    /// SQL语句接口
+    /// 作者：程淮榕
+    /// 时间：2018-11-06
+    /// </summary>
+    public interface ISetting
+    {
+        string Flag
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 数据域
+        /// </summary>
+        string DateFiled_sql   
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 对象是否包含(sql语句)
+        /// </summary>
+        string ColumnExists_sql   
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 对象是否包含（表名）
+        /// </summary>
+        string ColumnExists_tableName  
+        {
+            get;
+        }
+        /// <summary>
+        /// 对象是否包含（对象名）
+        /// </summary>
+        string ColumExists_columnName
+        {
+            get;
+        }
+        /// <summary>
+        /// 获取最大ID（sql语句）
+        /// </summary>
+        string GetMaxID_sql
+        {
+            get;
+        }
+        /// <summary>
+        /// 表是否包含（sql语句）
+        /// </summary>
+        string TabExists_sql
+        {
+            get;
+        }
+        /// <summary>
+        /// 获取对象时间
+        /// </summary>
+        string Column_GetDate
+        {
+            get;
+        }
+        /// <summary>
+        /// 获取服务器时间(sql语句)
+        /// </summary>
+        string GetServerDate_sql
+        {
+            get;
+        }
+        /// <summary>
+        /// 写入点击(sql语句)
+        /// </summary>
+        string WriteClick_sql
+        {
+            get;
+        }
+        /// <summary>
+        /// 批量删除(sql语句)
+        /// </summary>
+        string BantchDelete_sql
+        {
+            get;
+        }
+        /// <summary>
+        /// 删除对象(sql语句)
+        /// </summary>
+        string DeleteModel_sql
+        {
+            get;
+        }
+        /// <summary>
+        /// 删除对象(从集合中)
+        /// </summary>
+        string DeleteModel_sql_In
+        {
+            get;
+        }
+        /// <summary>
+        /// 删除对象（主键）
+        /// </summary>
+        string DeleteModel_PrimaryKey
+        {
+            get;
+        }
+        /// <summary>
+        /// 删除对象(主键列表)
+        /// </summary>
+        string DeleteModel_primaryKeyList
+        {
+            get;
+        }
+        /// <summary>
+        /// 删除对象（sql语句,通过WhereClip）
+        /// </summary>
+        string DeleteModel_sql_WhereClip
+        {
+            get;
+        }
+        /// <summary>
+        /// 选取首个对象(sql对象)
+        /// </summary>
+        string First_sql
+        {
+            get;
+        }
+        /// <summary>
+        /// 插入对象
+        /// </summary>
+        string Insert_sql
+        {
+            get;
+        }
+    }
+}
