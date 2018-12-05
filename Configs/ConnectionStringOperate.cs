9bfe24996dc3a93e8effee3c32bcadc87fc45d38
chr <intothesun@163.com>
2018-12-05 19:53:51 +0800

    操作类

--- Configs/ConnectionStringOperate.cs
+++ Configs/ConnectionStringOperate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Configs
 {
@@ -11,6 +12,31 @@ namespace Configs
     /// </summary>
      public class ConnectionStringOperate
     {
+        private static Dictionary<string, DBConnectionStringModel> dicDBConnectionModel_ = null;
 
+        /// <summary>
+        /// 数据库链接字典集
+        /// </summary>
+        private static Dictionary<string,DBConnectionStringModel> DicDBConnectionModel
+        {
+            get
+            {
+                if(dicDBConnectionModel_ == null)
+                {
+                    AppConfigurationService appConfigurationService = new AppConfigurationService(); //新建一个配置读取类实例
+                    List<DBConnectionStringModel> lstDbConnectionStringModel = appConfigurationService.GetAppSetting<List<DBConnectionStringModel>>("ConnectionStrings", "appsettings.json"); //读取配置模型集合
+                    dicDBConnectionModel_ = new Dictionary<string, DBConnectionStringModel>();
+                    foreach (var item in lstDbConnectionStringModel)
+                    {
+                        //将项目添加到字典
+                        dicDBConnectionModel_.Add(item.ConnectionName, item);
+                    }
+                }
+            }
+            set
+            {
+
+            }
+        }
     }
 }
