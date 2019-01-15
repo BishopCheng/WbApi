cef26ff314aecdad5059346d163ee4da8a5d89cf
chr <intothesun@163.com>
2019-01-15 16:55:22 +0800

    InfomationSite站点移入项目

---
+++ SiteSystem/Controllers/InitController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Business.Account;
+using ApiServer.Entity.TableModel.ADO;
+
+namespace Infomation.Controllers
+{
+    public class InitController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Init()
+        {
+            return View();
+        }
+       
+       
+
+        [HttpPost]
+        public ActionResult Login(IFormCollection form)
+        {
+            //获取参数
+            string userName = form["userName"];
+            string passWord = form["password"];
+            string validateCode = form["validateCode"];
+
+            t_user userModel = new t_user();
+
+            //调用业务逻辑层
+            LoginService loginService = new LoginService();
+            string msg = "";
+            msg = loginService.Login(userName, passWord, validateCode, ref userModel);
+            return Content(msg);
+        }
+
+        /// <summary>
+        /// 进入桌面
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult CMSDesk()
+        {
+            return View();
+        }
+    }
+}
---
+++ SiteSystem/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace Infomation
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            //注入视图模板引擎
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaViewLocationFormats.Clear();   //清空视图模板引擎
+                options.AreaViewLocationFormats.Add("/Areas/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Areas/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/{2}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name:"areaRoute",
+                    template:"{area:exists}/{controller}/{action}/{id?}"
+                    );
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
